7af7cf460f289b61ed8d278983939e9e52a9ddae
mernashenouda596-gif <mernashenouda596@gmail.com>
2026-07-20 17:30:02 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,77 @@
 import Navbar from './components/Navbar';
 
-import AnimatedBackground from './components/AnimatedBackground';
-// لو اسم الملف عندك AnimatedBackground استخدمي السطر اللي تحته بدل ParticleBackground:
-// import AnimatedBackground from './components/AnimatedBackground';
+import ParticleBackground from './components/ParticleBackground';
 
 import Hero from './components/Hero';
+
 import About from './components/About';
+
 import Skills from './components/Skills';
+
 import Statistics from './components/Statistics';
+
 import WorkProcess from './components/WorkProcess';
+
 import LearningJourney from './components/LearningJourney';
+
 import Projects from './components/Projects';
+
 import Contact from './components/Contact';
+
 import CTA from './components/CTA';
+
 import PersonalQuote from './components/PersonalQuote';
+
 import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className="relative min-h-screen overflow-x-hidden page-bg text-white">
-      
-      {/* 🌟 الخلفية المتحركة للموقع كله متثبتة */}
-     {/* 🌟 الخلفية المتحركة للموقع كله */}
-<div className="fixed inset-0 pointer-events-none z-0">
-  <AnimatedBackground />
-</div>
 
-      {/* باقي أجزاء الموقع فوق الخلفية */}
-      <div className="relative z-10">
+
+function App() {
+
+  return (
+
+    <div className="relative min-h-screen overflow-x-hidden page-bg">
+
+      <ParticleBackground />
+
+      <div className="relative" style={{ zIndex: 1 }}>
+
         <Navbar />
+
         <main>
+
           <Hero />
+
           <About />
+
           <Skills />
+
           <Statistics />
+
+          <WorkProcess />
+
           <LearningJourney />
+
           <Projects />
+
           <PersonalQuote />
+
           <Contact />
+
           <CTA />
+
         </main>
+
         <Footer />
+
       </div>
 
     </div>
+
   );
+
 }
+
+
 
 export default App;
